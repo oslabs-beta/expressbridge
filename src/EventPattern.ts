0b349b882a8ae214555885d35af409e2fe56f219
Sheila Wang <sheilaw@gmail.com>
2021-12-15 23:18:45 -0500

    first code

---
+++ src/EventPattern.ts
@@ -0,0 +1,36 @@
+ /* eslint-disable */
+type handlerType = () => void;
+type errorHandlerType = handlerType;
+
+class EventPattern<PatternType> {
+
+  constructor(
+    private pattern: Partial<PatternType>,
+    private handlers: handlerType[], 
+    private errorHandler: errorHandlerType = () => {}
+  ) {}
+
+  public getPattern(): Partial<PatternType> {
+    return this.pattern;
+  }
+
+  public getHandlers(): handlerType[] {
+    return this.handlers;
+  }
+
+  public getErrorHandler(): errorHandlerType {
+    return this.errorHandler;
+  }
+
+  public setPattern(pattern: Partial<PatternType>): void {
+    this.pattern = pattern;
+  }
+
+  public setHandlers(handlers: handlerType[]): void {
+    this.handlers = handlers;
+  }
+
+  public setErrorHandler(errorHandler: errorHandlerType): void {
+    this.errorHandler = errorHandler;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+/*
 export class HelloWorld {
   getGreeting(theName: String): string {
     return `Hello ${theName}`;
   }
 }
-
+*/
 const instance = new HelloWorld();
 console.log(instance.getGreeting('Example'));
